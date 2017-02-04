c961ff392c89a03e032c4dc767ddb104b1fcca7d
Alexey Korchevsky <mitallast@gmail.com>
2017-02-04 19:30:40 +0300

    improve rest resource handler

--- src/main/java/org/github/mitallast/taskflow/rest/handler/ResourceHandler.java
+++ src/main/java/org/github/mitallast/taskflow/rest/handler/ResourceHandler.java
@@ -8,9 +8,11 @@ import io.netty.handler.codec.http.HttpMethod;
 import org.github.mitallast.taskflow.common.component.AbstractComponent;
 import org.github.mitallast.taskflow.rest.RestController;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class ResourceHandler extends AbstractComponent {
 
@@ -22,51 +24,67 @@ public class ResourceHandler extends AbstractComponent {
         ImmutableSet<ClassPath.ResourceInfo> resources = classPath.getResources();
 
         resources.stream()
-            .map(resource -> {
-                logger.info(resource);
-                return resource;
-            })
             .filter(resource -> resource.getResourceName().startsWith("META-INF/resources/webjars/"))
             .forEach(resource -> {
                 String resourcePath = resource.getResourceName().substring("META-INF".length());
                 logger.trace("register {}", resourcePath);
-                controller.handler(this::resource)
+                controller.handler(this::webjars)
                     .param1(controller.param().path())
-                    .response(controller.response().optionalUrl())
+                    .response(controller.response().url())
                     .handle(HttpMethod.GET, resourcePath);
             });
 
-        resources.stream()
-            .filter(resource -> resource.getResourceName().startsWith("org/github/mitallast/taskflow/static"))
-            .forEach(resource -> {
-                String resourcePath = resource.getResourceName().substring("org/github/mitallast/taskflow/".length());
-                logger.info("register {}", resourcePath);
-                controller.handler(this::resource)
-                    .param1(controller.param().path())
-                    .response(controller.response().optionalUrl())
-                    .handle(HttpMethod.GET, resourcePath);
-            });
+        if (false) {
+            resources.stream()
+                .filter(resource -> resource.getResourceName().startsWith("org/github/mitallast/taskflow/static"))
+                .forEach(resource -> {
+                    String resourcePath = resource.getResourceName().substring("org/github/mitallast/taskflow/".length());
+                    logger.info("register {}", resourcePath);
+                    controller.handler(this::resourceStatic)
+                        .param1(controller.param().path())
+                        .response(controller.response().url())
+                        .handle(HttpMethod.GET, resourcePath);
+                });
+        } else {
+            Path root = new File("./src/main/resources/org/github/mitallast/taskflow/static").toPath();
+            Files.walk(root)
+                .filter(path -> path.toFile().isFile())
+                .forEach(path -> {
+                    StringBuilder builder = new StringBuilder();
+                    for (Path part : root.getParent().relativize(path)) {
+                        if (builder.length() > 0) {
+                            builder.append('/');
+                        }
+                        builder.append(part.getFileName());
+                    }
+                    String resourcePath = builder.toString();
 
-        logger.info("register favicon.ico");
-        controller.handler(this::resource)
-            .param1(controller.param().path())
-            .response(controller.response().optionalUrl())
+                    logger.info("register {}", resourcePath);
+                    controller.handler(this::fileStatic)
+                        .param1(controller.param().path())
+                        .response(controller.response().file())
+                        .handle(HttpMethod.GET, resourcePath);
+                });
+        }
+
+        controller.handler(this::favicon)
+            .response(controller.response().url())
             .handle(HttpMethod.GET, "favicon.ico");
     }
 
-    public Optional<URL> resource(String path) {
-        logger.trace("try find {}", path);
-        URL url;
-        if (path.startsWith("/resources/webjars/")) {
-            url = getClass().getResource("/META-INF" + path);
-        } else if (path.startsWith("/static/")) {
-            url = getClass().getResource("/org/github/mitallast/taskflow/" + path);
-        } else if (path.startsWith("/favicon.ico")) {
-            url = getClass().getResource("/favicon.ico");
-        } else {
-            url = null;
-        }
+    public URL webjars(String path) {
+        return getClass().getResource("/META-INF" + path);
+    }
 
-        return Optional.ofNullable(url);
+    public URL resourceStatic(String path) {
+        return getClass().getResource("/org/github/mitallast/taskflow/" + path);
+    }
+
+    public File fileStatic(String path) {
+        return new File("src/main/resources/org/github/mitallast/taskflow", path);
+    }
+
+    public URL favicon() {
+        return getClass().getResource("/favicon.ico");
     }
 }
